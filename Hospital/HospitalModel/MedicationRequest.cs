4c53b9aa5719f426b07f0ed0ef69121b14578fbe
Ksenia <lelsene@mail.ru>
2019-04-20 22:50:16 +0400

    Models v1.0

---
+++ Hospital/HospitalModel/MedicationRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HospitalModel
+{
+    public class MedicationRequest
+    {
+        public int Id { get; set; }
+
+        public int MedicationId { get; set; }
+
+        public int RequestId { get; set; }
+
+        [Required]
+        public int CountMedications { get; set; }
+
+        public virtual Medication Medication { get; set; }
+
+        public virtual Request Request { get; set; }
+    }
+}
