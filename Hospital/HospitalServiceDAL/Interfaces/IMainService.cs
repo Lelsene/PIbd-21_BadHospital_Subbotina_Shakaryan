412cad0ed6f8c7e4e393013677a81951c40fa239
Ksenia <lelsene@mail.ru>
2019-05-12 20:50:56 +0400

    Interfaces v1.0

---
+++ Hospital/HospitalServiceDAL/Interfaces/IMainService.cs
@@ -0,0 +1,13 @@
+﻿using HospitalServiceDAL.BindingModels;
+using HospitalServiceDAL.ViewModels;
+using System.Collections.Generic;
+
+namespace HospitalServiceDAL.Interfaces
+{
+    public interface IMainService
+    {
+        List<RequestViewModel> GetList();
+
+        void CreateRequest(RequestBindingModel model);
+    }
+}
