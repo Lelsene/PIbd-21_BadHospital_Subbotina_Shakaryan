5be92cc4ec9e19795f4d981d2d0b0f068e1cea8d
Ksenia <lelsene@mail.ru>
2019-05-12 18:40:26 +0400

    BindingModels v1.0

---
+++ Hospital/HospitalServiceDAL/BindingModels/MedicationBindingModel.cs
@@ -0,0 +1,13 @@
+﻿namespace HospitalServiceDAL.BindingModels
+{
+    public class MedicationBindingModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public int Count { get; set; }
+    }
+}
---
+++ Hospital/HospitalServiceDAL/BindingModels/MedicationRequestBindingModelcs.cs
@@ -0,0 +1,13 @@
+﻿namespace HospitalServiceDAL.BindingModels
+{
+    public class MedicationRequestBindingModelcs
+    {
+        public int Id { get; set; }
+
+        public int MedicationId { get; set; }
+
+        public int RequestId { get; set; }
+
+        public int CountMedications { get; set; }
+    }
+}
